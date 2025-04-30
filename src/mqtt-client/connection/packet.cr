e0c998bd3a87ae0302c6f850723ea6d91addebdb
Carl Hörberg <carl@84codes.com>
2025-04-30 12:18:12 +0200

    more compact Packets using records

--- src/mqtt-client/connection/packet.cr
+++ src/mqtt-client/connection/packet.cr
@@ -1,61 +1,15 @@
 module MQTT
   class Client
-    abstract struct Packet
-    end
+    abstract struct Packet; end
 
-    struct Subscribe < Packet
-      getter topics
-
-      def initialize(@topics : Enumerable(Tuple(String, UInt8)))
-      end
-    end
-
-    struct PingReq < Packet
-    end
-
-    struct PubAck < Packet
-      getter packet_id
-
-      def initialize(@packet_id : UInt16)
-      end
-    end
-
-    struct PubRec < Packet
-      getter packet_id
-
-      def initialize(@packet_id : UInt16)
-      end
-    end
-
-    struct PubRel < Packet
-      getter packet_id
-
-      def initialize(@packet_id : UInt16)
-      end
-    end
-
-    struct PubComp < Packet
-      getter packet_id
-
-      def initialize(@packet_id : UInt16)
-      end
-    end
-
-    struct Disconnect < Packet
-    end
-
-    struct Unsubscribe < Packet
-      getter topics
-
-      def initialize(@topics : Enumerable(String))
-      end
-    end
-
-    struct Publish < Packet
-      getter topic, body, qos, retain, dup
-
-      def initialize(@topic : String, @body : Bytes, @qos : UInt8, @retain : Bool, @dup : Bool)
-      end
-    end
+    record Subscribe < Packet, topics : Enumerable(Tuple(String, UInt8))
+    record PingReq < Packet
+    record PubAck < Packet, packet_id : UInt16
+    record PubRec < Packet, packet_id : UInt16
+    record PubRel < Packet, packet_id : UInt16
+    record PubComp < Packet, packet_id : UInt16
+    record Disconnect < Packet
+    record Unsubscribe < Packet, topics : Enumerable(String)
+    record Publish < Packet, topic : String, body : Bytes, qos : UInt8, retain : Bool, dup : Bool
   end
 end
